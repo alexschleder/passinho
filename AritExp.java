6af0fd96476a723dee5159df23880416b7f024d6
Conrado <bruno.conrado@acad.pucrs.br>
2021-10-05 01:14:32 -0300

    finish aritExp

--- AritExp.java
+++ AritExp.java
@@ -3,33 +3,31 @@ public class AritExp extends Exp {
   Exp exp2;
   OpArit op;
 
-  public AritExp(Exp exp1){
-    this.exp1 = exp1;
-  }
-
-  public AritExp(Exp exp1, Exp exp2, OpArit op){
+  public AritExp(Exp exp1, OpArit op, Exp exp2) {
     this.exp1 = exp1;
     this.exp2 = exp2;
     this.op = op;
   }
 
-  public AritExp(AritExp exp1, Exp exp2, OpArit op ){
-    
-  }
-
-  public AritExp(AritExp exp1, AritExp exp2, OpArit op ){
-    
-  }
-
-
-
   @Override
-    Exp smallstep(Estado e) {
-        return this;
+  Exp smallstep(Estado e) {
+    if (!(exp1 instanceof Num)) {
+      return new AritExp(exp1.smallstep(e), op, exp2);
+    } else if (!(exp2 instanceof Num)) {
+      return new AritExp(exp1, op, exp2.smallstep(e));
+    } else {
+      if (op.toString().equals("+")) {
+        return new Num(((Num) exp1).getValor() + ((Num) exp2).getValor());
+      } else if (op.toString().equals("-")) {
+        return new Num(((Num) exp1).getValor() - ((Num) exp2).getValor());
+      } else { // op == *
+        return new Num(((Num) exp1).getValor() * ((Num) exp2).getValor());
+      }
     }
+  }
 
   @Override
   public String toString() {
-      return "";
+    return "";
   }
 }
--- BoolExp.java
+++ BoolExp.java
@@ -1,62 +1,62 @@
-public class BoolExp extends Exp{
+public class BoolExp extends Exp {
 
     private Exp exp1;
     private Exp exp2;
     private Exp op;
 
-    public BoolExp(Exp exp1, Exp op, Exp exp2){
+    public BoolExp(Exp exp1, Exp op, Exp exp2) {
         this.exp1 = exp1;
         this.op = op;
         this.exp2 = exp2;
     }
 
-    public BoolExp(Exp op, Exp exp1){ //!b
+    public BoolExp(Exp op, Exp exp1) { // !b
         this.op = op;
         this.exp1 = exp1;
     }
 
-    // 𝑏 ∷= 𝑏𝑙 | 𝑥 | 𝑏1 𝑏𝑜𝑝 𝑏2 | !𝑏 | 𝑎1 𝑟𝑜𝑝 𝑎2 
+    // 𝑏 ∷= 𝑏𝑙 | 𝑥 | 𝑏1 𝑏𝑜𝑝 𝑏2 | !𝑏 | 𝑎1 𝑟𝑜𝑝 𝑎2
 
     @Override
-    public String toString(){
+    public String toString() {
         return exp1 + op.toString() + exp2;
     }
 
     @Override
-    Exp smallstep(Estado e){
-        if(op.toString().equals("!")){
-            if(!(exp1 instanceof Bool)){
+    Exp smallstep(Estado e) {
+        if (op.toString().equals("!")) {
+            if (!(exp1 instanceof Bool)) {
                 return new BoolExp(op, exp1.smallstep(e));
-            }else{
-                return new Bool(!((Bool)exp1).getValorBoolean());
+            } else {
+                return new Bool(!((Bool) exp1).getValorBoolean());
             }
         }
-        if(op instanceof OpBool){
-                if(!(exp1 instanceof Bool)){
-                    return new BoolExp(exp1.smallstep(e), op, exp2);
-                }else if(!(exp2 instanceof Bool)){
-                    return new BoolExp(exp1, op, exp2.smallstep(e));
-                }else{
-                    if(op.toString().equals("&&")){
-                        return new Bool(((Bool)exp1).getValorBoolean() && ((Bool)exp2).getValorBoolean());
-                    }else{
-                        return new Bool(((Bool)exp1).getValorBoolean() || ((Bool)exp2).getValorBoolean());
-                    }
-                }
-            }else{ //OpRel
-                if(!(exp1 instanceof Num)){ //isso já confere se é num ou AritExp?
-                    return new BoolExp(exp1.smallstep(e), op, exp2);
-                }else if(!(exp2 instanceof Num)){
-                    return new BoolExp(exp1, op, exp2.smallstep(e));
-                }else{
-                    if(op.toString().equals("=")){ //if pra cada comparação: > < =
-                        return new Bool(((Num)exp1).getValor() == ((Num)exp2).getValor());
-                    }else if(op.toString().equals(">")){
-                        return new Bool(((Num)exp1).getValor() > ((Num)exp2).getValor());
-                    }else{
-                        return new Bool(((Num)exp1).getValor() < ((Num)exp2).getValor());
-                    }
+        if (op instanceof OpBool) {
+            if (!(exp1 instanceof Bool)) {
+                return new BoolExp(exp1.smallstep(e), op, exp2);
+            } else if (!(exp2 instanceof Bool)) {
+                return new BoolExp(exp1, op, exp2.smallstep(e));
+            } else {
+                if (op.toString().equals("&&")) {
+                    return new Bool(((Bool) exp1).getValorBoolean() && ((Bool) exp2).getValorBoolean());
+                } else {
+                    return new Bool(((Bool) exp1).getValorBoolean() || ((Bool) exp2).getValorBoolean());
                 }
             }
+        } else { // OpRel
+            if (!(exp1 instanceof Num)) { // isso já confere se é num ou AritExp?
+                return new BoolExp(exp1.smallstep(e), op, exp2);
+            } else if (!(exp2 instanceof Num)) {
+                return new BoolExp(exp1, op, exp2.smallstep(e));
+            } else {
+                if (op.toString().equals("=")) { // if pra cada comparação: > < =
+                    return new Bool(((Num) exp1).getValor() == ((Num) exp2).getValor());
+                } else if (op.toString().equals(">")) {
+                    return new Bool(((Num) exp1).getValor() > ((Num) exp2).getValor());
+                } else {
+                    return new Bool(((Num) exp1).getValor() < ((Num) exp2).getValor());
+                }
+            }
+        }
     }
 }
