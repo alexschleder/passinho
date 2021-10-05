a7479d4eaa063c2a5dd00bc7beb5031295fbf9f0
Alexandre Ribeiro Schleder de Borba <alexandre.schleder@gmail.com>
2021-10-04 23:19:16 -0300

    Ta certo agora

--- Estado.java
+++ Estado.java
@@ -1,25 +1,31 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 public class Estado {
-    private Map<String, Integer> mapeamento = new HashMap<>();
+    private Map<String, Integer> mapeamentoInt = new HashMap<>();
+    private Map<String, Boolean> mapeamentoBool = new HashMap<>();
     /**
      * Adiciona uma nova variável ao ambiente.
      * @param variavel
      * @param valor
      */
     public void adicionar(String variavel, int valor) {
-        mapeamento.put(variavel,valor);
+        mapeamentoInt.put(variavel,valor);
+        mapeamentoBool.remove(variavel);
     }
 
-    /**
-     * Atualiza o valor de uma variável no ambiente.
-     * Assume a variável já existe no ambiente.
-     * @param variavel
-     * @param valor
-     */
-    public void atualizar(String variavel, int valor) {
-        mapeamento.replace(variavel,valor);
+    public void adicionar(String variavel, boolean valor) {
+        mapeamentoBool.put(variavel,valor);
+        mapeamentoInt.remove(variavel);
+    }
+
+    public Tipo lerTipo(String variavel)
+    {
+        if (mapeamentoInt.get(variavel) != null) return Tipo.integer;
+        if (mapeamentoBool.get(variavel) != null) return Tipo.bool;
+        return Tipo.undefined;
     }
 
     /**
@@ -28,20 +34,35 @@ public class Estado {
      * @param variavel
      * @return valor da variável
      */
-    public int ler(String variavel) {
-        return mapeamento.get(variavel);
+    public int lerInt(String variavel) {
+        return mapeamentoInt.get(variavel);
+    }
+
+    public boolean lerBool(String variavel) {
+        return mapeamentoBool.get(variavel);
     }
 
     @Override
     public String toString() {
         StringBuilder ambiente = new StringBuilder("[");
-        var iterador = mapeamento.entrySet().iterator();
+        Iterator<Entry<String,Integer>> iterador = mapeamentoInt.entrySet().iterator();
         while (iterador.hasNext()) {
-            var e = iterador.next();
+            Entry<String,Integer> e = iterador.next();
             ambiente.append(e.getKey());
             ambiente.append(" |-> ");
             ambiente.append(e.getValue());
             if (iterador.hasNext()) {
+                ambiente.append(", ");
+            }
+        }
+        ambiente.append("\n");
+        Iterator<Entry<String,Boolean>> iterador2 = mapeamentoBool.entrySet().iterator();
+        while (iterador2.hasNext()) {
+            Entry<String,Boolean> e = iterador2.next();
+            ambiente.append(e.getKey());
+            ambiente.append(" |-> ");
+            ambiente.append(e.getValue());
+            if (iterador2.hasNext()) {
                 ambiente.append(", ");
             }
         }
